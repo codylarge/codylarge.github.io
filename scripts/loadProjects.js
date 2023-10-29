fe9d8ea1aa6ec0811fcbf53b496a5cbfe743218b
Cody Large <dlarge1@ewu.edu>
2023-10-28 23:08:11 -0700

    Added Much More
    Mostly all pages added except for about page

--- scripts/loadProjects.js
+++ scripts/loadProjects.js
@@ -1,21 +1,65 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Array of image data with image URLs and titles
+  // TODO: Make them all jpgs
   const imageData = [
+    {
+      src: "../img/art/Arcade.png",
+      alt: "Mordecai Arcade Game",
+      title: "Mordecai Arcade",
+      link: "https://example.com/Arcade",
+    },
+    {
+      src: "../img/art/Magnemite.png",
+      alt: "Magnemite",
+      title: "Magnemite",
+      link: "https://example.com/Magnemite",
+    },
     {
       src: "../img/art/Larry.png",
       alt: "Larry The Cucumber",
       title: "Larry The Pirate",
+      link: "https://example.com/Larry",
     },
-    { src: "../img/art/Magnemite.png", alt: "Magnemite", title: "Magnemite" },
+    {
+      src: "../img/art/Pirates.png",
+      alt: "The Pirates Who Don't Do Anything Veggie Tales",
+      title: "VeggieTales Album Cover",
+      link: "https://example.com/Pirates",
+    },
+    {
+      src: "../img/art/Koffing.jpg",
+      alt: "Koffing",
+      title: "Koffing",
+      link: "https://example.com/Koffing",
+    },
+    {
+      src: "../img/art/Rainbow.png",
+      alt: "Rainbow",
+      title: "Rainbow",
+      link: "https://example.com/Rainbow",
+    },
+    {
+      src: "../img/art/MushroomUFO.png",
+      alt: "MushroomUFO",
+      title: "Mushroom UFO",
+      link: "https://example.com/MushroomUFO",
+    },
+    {
+      src: "../img/art/Drugs R Bad.jpg",
+      alt: "Drugs R Bad",
+      title: "Drugs R Bad",
+      link: "https://example.com/Rainbow",
+    },
     // Add more image data objects as needed
   ];
 
   const imageContainer = document.getElementById("imageContainer");
 
-  // Loop through the image data and create image divs dynamically
   imageData.forEach((image) => {
     const div = document.createElement("div");
     div.classList.add("item");
+
+    const link = document.createElement("a");
+    link.href = image.link; // Set the link
 
     const img = document.createElement("img");
     img.src = image.src;
@@ -24,7 +68,8 @@ document.addEventListener("DOMContentLoaded", function () {
     const title = document.createElement("h2");
     title.textContent = image.title;
 
-    div.appendChild(img);
+    link.appendChild(img); // Append the image to the anchor element
+    div.appendChild(link); // Append the anchor to the container
     div.appendChild(title);
     imageContainer.appendChild(div);
   });
---
+++ scripts/loadVideos.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const videoData = [
+    // ADD: JW Player, JDBC Project, Stack,
+    {
+      src: "../img/cs/SuperSmash.mp4",
+      title: "Mega Smash Sisters",
+      link: "https://github.com/codylarge/Mega-Smash-Sisters",
+    },
+    {
+      src: "../img/cs/TTWebapp.mp4",
+      title: "Toontown Calculator",
+      link: "https://github.com/codylarge/Toontown-Webapp",
+    },
+    // Add more video data objects as needed
+  ];
+
+  const videoContainer = document.getElementById("videoContainer");
+
+  videoData.forEach((video) => {
+    const div = document.createElement("div");
+    div.classList.add("item");
+
+    const title = document.createElement("h2");
+    title.textContent = video.title;
+
+    const link = document.createElement("a");
+    link.href = video.link;
+    link.appendChild(title);
+
+    const vid = document.createElement("video");
+    vid.src = video.src;
+    vid.controls = true;
+    vid.muted = true;
+
+    div.appendChild(link);
+    div.appendChild(vid);
+    videoContainer.appendChild(div);
+
+    title.addEventListener("click", function (event) {
+      event.stopPropagation(); // Prevents the click from reaching the parent link
+      window.location.href = video.link; // Directs to the link when h2 is clicked
+    });
+  });
+});
